3241618e2328a8133a364477f8d71b23b3b20275
Alex <alex@example.com>
2024-12-12 20:49:37 +0300

    first vue

--- learn/index.js
+++ learn/index.js
@@ -1,1 +1,15 @@
-alert(121112333)
+// Получаем элементы из DOM
+// const btn = document.getElementById('btn');
+// const div = document.getElementById('likeCount');
+
+// let counter = 0;
+
+// btn.addEventListener('click', () => {
+//     counter += 1;
+//     div.innerText = `Кол-во лайков = ${counter}`
+// })
+
+//Создайте метод, который принимает массив имен и возвращает массив каждого имени с заглавной первой буквой.
+
+
+
---
+++ my-project/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="app">
+        <div>
+            <post-form 
+            @create="createPost"
+            />
+            <post-list 
+            :posts="posts"
+            />
+            
+            
+        </div>
+
+        
+    </div>
+</template>
+
+<script>
+import PostForm from './components/PostForm.vue';
+import PostList from './components/PostList.vue';
+
+export default {
+    components: {
+        PostList, PostForm
+    },
+    data(){
+        return {
+            posts: [
+            {id: 1, title: 'JavaScript 1', body: 'Описание поста 1'},
+            {id: 2, title: 'JavaScript 2', body: 'Описание поста 2'},
+            {id: 3, title: 'JavaScript 3', body: 'Описание поста 3'},
+            {id: 4, title: 'JavaScript 4', body: 'Описание поста 4'},
+        ],
+        }
+    },
+    methods: {
+        createPost(post){
+            this.posts.push(post);
+        },
+    }
+}
+</script>
+
+<style>
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    .app {
+        padding: 20px;
+    }
+
+
+
+    
+</style>
